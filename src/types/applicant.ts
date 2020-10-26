419c00b06dcc223181dfbd07f40fcb7cae8b98b3
Kavpreet Grewal <kavpreetgrewal@gmail.com>
2020-10-25 20:24:47 -0700

    Updated applicant interface

--- src/types/applicant.ts
+++ src/types/applicant.ts
@@ -1,48 +1,58 @@
-import {getData, addApplicant} from '../utils/gsuite/sheets';
+import { getData, addApplicant } from '../utils/gsuite/sheets';
 
 export interface Applicant {
-    name: string,
-    email: string,
-    year: number,
-    major: string,
-    platforms: string,
-    resume: string,
-    website?: string
+    firstName: string;
+    lastName: string;
+    email: string;
+    year: number;
+    major: string;
+    role: string;
+    resume: string;
+    website?: string;
+    linkedin?: string;
 }
 
 // creates an instance of an applicant with the given parameters
-function createApplicant(name: string, email: string, year: number, major: string, platforms: string, resume: string,
-                         website?: string) {
-    let applicant: Applicant = {name, email, year, major, platforms, resume, website};
+function createApplicant(
+    firstName: string,
+    lastName: string,
+    email: string,
+    year: number,
+    major: string,
+    role: string,
+    resume: string,
+    website?: string,
+    linkedin?: string,
+) {
+    const applicant: Applicant = { firstName, lastName, email, year, major, role, resume, website, linkedin };
     return applicant;
 }
 
 // creates a list of applicants by parsing google sheets with given url, sheet name, and data range
 // range has form 'A1:G12'
 async function setApplicants(sheetUrl: string, sheetName: string, range: string) {
-    let applicants: Array<Applicant> = [];
-    let data = await getData(sheetUrl, sheetName, range);
+    const applicants: Array<Applicant> = [];
+    const data = await getData(sheetUrl, sheetName, range);
 
-    data.map((row) =>{
-        let applicant = createApplicant(row[0], row[1], row[2], row[3], row[4], row[5], row[6]);
+    data.map((row) => {
+        const applicant = createApplicant(row[0], row[1], row[4], row[5], row[3], row[2], row[6], row[7], row[8]);
         applicants.push(applicant);
     });
 
     return applicants;
 }
 
-
 // url of sample data sheet with applicants
-let dataSheetUrl = 'https://docs.google.com/spreadsheets/d/1JgrBy8aWRdGOsFKgXtylsEHFJdKfTWD0tE_cansjpU4/edit#gid=0';
+// let dataSheetUrl = 'https://docs.google.com/spreadsheets/d/1JgrBy8aWRdGOsFKgXtylsEHFJdKfTWD0tE_cansjpU4/edit#gid=0';
+const dataSheetUrl = 'https://docs.google.com/spreadsheets/d/1knAAeS1sn6GsT5V12nrZo-2Ma9dyPGVoOGphhSBq0zo/edit#gid=0';
 
 // creating list of applicants
-setApplicants(dataSheetUrl, 'Sheet1', 'A2:G12').then(((applicants) => {
+setApplicants(dataSheetUrl, 'Sheet1', 'A2:I15').then((applicants) => {
     console.log(applicants);
-}))
+});
 
 // creates new test applicant
-let newApplicant = createApplicant("Test Person7", "test@test.com", 5, "cs", "iOS",
-    "test.com")
+const newApplicant = createApplicant('Test15', 'Person15', 'test@test.com', 5, 'cs', 'iOS', 'test.com');
 
 // adds applicant to the sheet after the end of the given range
-addApplicant(dataSheetUrl, 'Sheet1', 'A2:G12', newApplicant);
+addApplicant(dataSheetUrl, 'Sheet1', 'A2:I15', newApplicant).then((res) => console.log(res));
