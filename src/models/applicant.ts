a974688ebcd8d39fd9aecad4991bc463924152cf
Jed <jed324@gmail.com>
2020-10-28 10:57:51 -0700

    lint

--- src/models/applicant.ts
+++ src/models/applicant.ts
@@ -1,47 +1,45 @@
 import * as mongoose from 'mongoose';
-import {Schema} from 'mongoose';
+import { Schema } from 'mongoose';
 
-const applicantSchema = new Schema(
-    {
-        firstName: {
-            type: String,
-            trim: true,
-            required: true,
-        },
-        lastName: {
-            type: String,
-            trim: true,
-            required: true,
-        },
-        role: {
-            type: Number,
-            enum: ['Developer', 'Designer'],
-            default: 0,
-            required: true,
-        },
-        major: {
-            type: String,
-            required: true,
-        },
-        yearStanding: {
-            type: Number,
-            required: true,
-        },
-        linkedIn: {
-            type: String,
-            required: false,
-        },
-        website: {
-            type: String,
-            required: false,
-        },
-        resume: {
-            type: String,
-            required: false,
-        }
-    }
-);
+const applicantSchema = new Schema({
+    firstName: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    role: {
+        type: Number,
+        enum: ['Developer', 'Designer'],
+        default: 0,
+        required: true,
+    },
+    major: {
+        type: String,
+        required: true,
+    },
+    yearStanding: {
+        type: Number,
+        required: true,
+    },
+    linkedIn: {
+        type: String,
+        required: false,
+    },
+    website: {
+        type: String,
+        required: false,
+    },
+    resume: {
+        type: String,
+        required: false,
+    },
+});
 
 const Applicant = mongoose.model('Applicant', applicantSchema);
 
-module.exports = Applicant
+module.exports = Applicant;
