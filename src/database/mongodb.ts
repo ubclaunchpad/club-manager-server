11b6711dfd3695a1b693fcc42205bd064f4281fe
Connor Fong <dconnorfong@gmail.com>
2020-11-19 00:16:14 -0800

    Fixed broken mongo connections for docker-compose (#41)

--- src/database/mongodb.ts
+++ src/database/mongodb.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const ENDPOINT = process.env.ENDPOINT_DB || 'mongodb://127.0.0.1:27017/clubmanager';
+const ENDPOINT = process.env.ENDPOINT_DB || 'mongodb://mongodb:27017/club-manager';
 
 mongoose.connect(ENDPOINT, {
     useCreateIndex: true,
