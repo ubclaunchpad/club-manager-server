220f0e942c703bff6fd56feeb1c98a92f3738c54
thomaslapadat <thomaslapadat@gmail.com>
2021-02-24 13:28:53 -0800

    Temporarily reverting authentication on applicant endpoints

--- src/routes/applicant.ts
+++ src/routes/applicant.ts
@@ -5,13 +5,13 @@ import { verifyToken } from '../middleware/auth-token';
 const router = express.Router();
 
 /* Returns a list of all applicants currently in the database */
-router.get('/', verifyToken, listAllApplicants);
+router.get('/', listAllApplicants);
 
 /* Turns the JSON object passed in the request body into a new applicant */
-router.post('/', verifyToken, createApplicant);
+router.post('/', createApplicant);
 
 /* Sets the status of the applicant matching 'id' */
-router.patch('/:id', verifyToken, updateApplicantFields);
+router.patch('/:id', updateApplicantFields);
 
 /* Endpoint used for sanity checking */
 router.get('/debug', verifyToken, (req, res) => {
