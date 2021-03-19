1c3f5b8a5a6064b4e94209ce078d30ca6f4bc4c4
Connor Fong <dconnorfong@gmail.com>
2021-03-19 16:52:16 -0700

    Updated mongoose file to support Atlas remote DB

--- src/database/mongodb.ts
+++ src/database/mongodb.ts
@@ -2,12 +2,16 @@ import mongoose from 'mongoose';
 
 const ENDPOINT = process.env.ENDPOINT_DB || 'mongodb://mongodb:27017/club-manager'; // try switching to mongodb://127.0.0.1:27017/club-manager if not working on a local instace
 
-mongoose.connect(ENDPOINT, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-});
+mongoose
+    .connect(ENDPOINT, {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    })
+    .catch((error) => {
+        console.log(`Error: ${error}`);
+    });
 
 mongoose.connection.once('open', () => {
     console.log(`Connected to database at ${ENDPOINT}`);
