8911d0bd70515b644c44ece185763931bf3b57a5
Leqi(Nancy) Wan <leqiwan@alumni.ubc.ca>
2021-03-23 14:12:59 +0800

    Adding emailGridRouter and POST method for sending email

---
+++ src/routes/email-grid.ts
@@ -0,0 +1,26 @@
+import { Request, Response, Router } from 'express';
+import { Mail } from '../types/mail';
+import { sendEmail } from '../utils/mail/sendgrid';
+
+export const emailGridRouter = Router();
+
+/**
+ * @description POST request to send an email from an authorized Gmail account
+ */
+ emailGridRouter.post('/send-grid', async (req: Request, res: Response) => {
+    try {
+        const mailMessage: Mail = {
+            from: req.body.from,
+            subject: req.body.subject,
+            text: req.body.text,
+            html: req.body.html
+        }
+        const recipient = req.body.recipient;
+        const resp = await sendEmail(recipient, mailMessage);
+        if (resp.status === 200) {
+            res.send('Mail sent successfully!');
+        }
+    } catch (e) {
+        res.status(404).send(e.message);
+    }
+});
--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,15 @@ import express from 'express';
 import * as bodyParser from 'body-parser';
 import cors from 'cors';
 import { emailRouter } from './routes/email';
+import { emailGridRouter } from './routes/email-grid';
 import { authRouter } from './routes/auth';
 import { ensureAuthenticated } from './utils/auth/auth-utils';
 import applicantRouter from './routes/applicant';
 import userRouter from './routes/user';
 import sheetsRouter from './routes/sheets';
 import gradeRouter from './routes/grade';
+
+import { sendEmail } from './utils/mail/sendgrid';
 
 dotenv.config();
 
@@ -28,7 +31,16 @@ server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 
 server.use('/api/auth', authRouter);
+server.use('/api/email', emailGridRouter);
 server.use('/api/email', ensureAuthenticated(), emailRouter);
+
+sendEmail('nancywan1004@gmail.com', {
+    from: 'ubc.launchpad.clubmanager@gmail.com',
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy!',
+    html: '<h1>and easy!</h1>',
+});
+
 // Establish DB connection
 require('./database/mongodb');
 
