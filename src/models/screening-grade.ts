bb4018c8e8be9ce85ea5b56d2e04f40f9423496d
Connor Fong <dconnorfong@gmail.com>
2021-03-30 16:03:55 -0700

    Updated screening grade to accurate busines requirements

--- src/models/screening-grade.ts
+++ src/models/screening-grade.ts
@@ -1,21 +1,18 @@
 import mongoose from 'mongoose';
+import { IApplicant } from './applicant';
 import { Document, Model, Schema } from 'mongoose';
 
 export interface IScreeningGrade extends Document {
-    applicant: { type: Schema.Types.ObjectId; ref: 'Applicant' };
+    applicant: IApplicant['_id'];
     c1: number; // Willingness to learn (self-drive/are you taking time out of classes to learn and develop?)
-    c2: number; // Passion and interest (eg comprehensiveness of answer to "tell us about a project", even if not technically competent)
+    c2: number; // Passion and interest (e.g. comprehensiveness of answer to "tell us about a project", even if not technically competent)
     c3: number; // Rate the best of the provided items from the OR/AND's from the relevant skill bucket they fall in
-    c4: number; // Beginner (put 2 if yes, put 0 if not) view Recruitment overview for criteria for beginner
-    c5: number; // Independent (put 2 if yes, put 0 if not) view Recruitment overview for criteria for independent
-    c6: number; // Experienced (put 2 if yes, put 0 if not) view Recruitment overview for criteria for experienced
     total: number;
 }
 
 const gradeSchema = new Schema({
     applicant: {
         type: Schema.Types.ObjectId,
-        ref: 'Applicant',
         required: true,
     },
     c1: {
@@ -31,24 +28,6 @@ const gradeSchema = new Schema({
         required: true,
     },
     c3: {
-        type: Number,
-        enum: [0, 1, 2],
-        default: 0,
-        required: true,
-    },
-    c4: {
-        type: Number,
-        enum: [0, 1, 2],
-        default: 0,
-        required: true,
-    },
-    c5: {
-        type: Number,
-        enum: [0, 1, 2],
-        default: 0,
-        required: true,
-    },
-    c6: {
         type: Number,
         enum: [0, 1, 2],
         default: 0,
--- src/routes/grade.ts
+++ src/routes/grade.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import { createInterviewGrade, getInterviewGrade, updateInterviewNumericalGrade } from '../controllers/interview-grade';
-import { createScreeningGrade, getScreeningGrade, updateScreeningNumericalGrade } from '../controllers/screening-grade';
+import { createScreeningGrade, getScreeningGrade } from '../controllers/screening-grade';
 
 const router = express.Router();
 
@@ -15,9 +15,6 @@ router.post('/screening/:applicantId/', createScreeningGrade);
 
 /* Turns the JSON object passed in the request body into a new interview report for the applicant */
 router.post('/interview/:applicantId/', createInterviewGrade);
-
-/* Sets the interview numerical grades of the applicant matching the applicantId in the query param */
-router.patch('/screening/:applicantId/', updateScreeningNumericalGrade);
 
 /* Sets the interview numerical grades of the applicant matching the applicantId in the query param */
 router.patch('/interview/:applicantId/', updateInterviewNumericalGrade);
